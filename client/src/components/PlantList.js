b4a944bff4c4d33cdf213f4755736c431d36df98
Ismail Al Kamal <ismail@nawatt.com>
2020-11-07 06:34:06 +0300

    Stretch #1 completed

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -7,6 +7,8 @@ export default class PlantList extends Component {
     super();
     this.state = {
       plants: [],
+      search: "",
+      results: [],
     };
   }
 
@@ -17,16 +19,45 @@ export default class PlantList extends Component {
     axios
       .get("http://localhost:3333/plants")
       .then((res) => {
-        this.setState({ plants: res.data.plantsData });
+        this.setState({
+          plants: res.data.plantsData,
+          results: res.data.plantsData,
+        });
       })
       .catch((err) => console.log(err));
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.search !== prevState.search)
+      this.setState({
+        ...this.state,
+        results: this.state.plants.filter(
+          (plant) =>
+            plant.name.toLowerCase().includes(this.state.search) ||
+            plant.scientificName.toLowerCase().includes(this.state.search) ||
+            plant.description.toLowerCase().includes(this.state.search)
+        ),
+      });
+  }
+
+  handleSearch = (e) => {
+    this.setState({
+      ...this.state,
+      search: e.target.value,
+    });
+  };
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
       <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
+        <input
+          type="text"
+          name="search"
+          value={this.state.search}
+          onChange={this.handleSearch}
+        />
+        {this.state?.results?.map((plant) => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
